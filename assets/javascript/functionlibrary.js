d6cf230d807349da205a906c04d11b18cfaa594a
James <jwin4740@gmail.com>
2016-12-19 23:01:15 -0500

    functionlib

---
+++ assets/javascript/functionlibrary.js
@@ -0,0 +1,167 @@
+function fIntro() {
+        $("#theme")[0].volume = 0.5;
+        $("#theme")[0].play();
+    }
+
+    function fIntro2() {
+        $("#theme2")[0].play();
+    }
+
+    function showArena() {
+        $("#arena:hidden:first").fadeIn(2000);
+    }
+
+    function hoversound() {
+        $("#hoversound")[0].volume = 0.6;
+        $("#hoversound")[0].play();
+
+    }
+
+    function fDraw() {
+        $("#saberdraw")[0].play();
+    }
+
+
+    function fBuzz() {
+        $("#saberbuzz")[0].play();
+    }
+
+    function sBuzz() {
+        $("#sabersbuzz")[0].play();
+    }
+
+    function fContact() {
+        $("#sabercontact")[0].play();
+    }
+
+    function fSheath() {
+        $("#sabersheath")[0].play();
+    }
+
+    function resetSound() {
+        $("#saberbuzz")[0].pause();
+        $("#sabersbuzz")[0].pause();
+        $("#saberbuzz")[0].currentTime = 0;
+        $("#sabersbuzz")[0].currentTime = 0;
+    }
+
+        function selectCharacter() {
+
+        function hoverEnlarge(idname) {
+
+            $(idname).on("mouseenter", function() {
+                $(idname).css({ border: "solid 5px yellow" });
+                hoversound();
+
+                $(idname).on("mouseleave", function() {
+                    $(idname).css({ border: "solid 1px white" });
+
+                });
+            });
+        }
+
+        hoverEnlarge(qui);
+        hoverEnlarge(darth);
+        hoverEnlarge(obi);
+        hoverEnlarge(count);
+    }
+
+     // makes player move with arrow keys
+    $(document).keyup(function(e) {
+        switch (e.which) {
+            case 37:
+                $(".obiwan").animate({ left: "-=50px" }, "normal");
+        }
+    });
+    $(document).keyup(function(e) {
+        switch (e.which) {
+            case 38:
+                $(".obiwan").animate({ top: "-=50px" }, "normal");
+        }
+    });
+
+    $(document).keyup(function(e) {
+        switch (e.which) {
+            case 39:
+                $(".obiwan").animate({ left: "+=50px" }, "normal");
+        }
+    });
+
+    $(document).keyup(function(e) {
+        switch (e.which) {
+            case 40:
+                $(".obiwan").animate({ top: "+=50px" }, "normal");
+
+        }
+    });
+
+
+function showPlayerObi() {
+        $("#halfleft").append('<img class="playerselect" src="assets/images/obiwan.jpg">');
+        $("#halfleft").css({ display: "block" });
+        $("#halfright").append('<img class="playerenemy" src="assets/images/darthmaul.jpeg">');
+        $("#halfright").css({ display: "block" });
+        $("#halfright").append('<img class="playerenemy" src="assets/images/quigon.jpeg">');
+        $("#halfright").css({ display: "block" });
+        $("#halfright").append('<img class="playerenemy" src="assets/images/countdooku.jpg">');
+        $("#halfright").css({ display: "block" });
+        player = true;
+        console.log(player);
+        if (player === true) {
+            console.log(player);
+            
+        }
+    }
+
+
+    function showPlayerQui() {
+        $("#halfleft").append('<img class="playerselect" src="assets/images/quigon.jpeg">');
+        $("#halfleft").css({ display: "block" });
+        $("#halfright").append('<img class="playerenemy" src="assets/images/darthmaul.jpeg">');
+        $("#halfright").css({ display: "block" });
+        $("#halfright").append('<img class="playerenemy" src="assets/images/obiwan.jpg">');
+        $("#halfright").css({ display: "block" });
+        $("#halfright").append('<img class="playerenemy" src="assets/images/countdooku.jpg">');
+        $("#halfright").css({ display: "block" });
+        player = true;
+        console.log(player);
+        if (player === true) {
+            console.log(player);
+           
+        }
+    }
+
+    function showPlayerCount() {
+        $("#halfleft").append('<img class="playerselect" src="assets/images/countdooku.jpg">');
+        $("#halfleft").css({ display: "block" });
+        $("#halfright").append('<img class="playerenemy" src="assets/images/darthmaul.jpeg">');
+        $("#halfright").css({ display: "block" });
+        $("#halfright").append('<img class="playerenemy" src="assets/images/obiwan.jpg">');
+        $("#halfright").css({ display: "block" });
+        $("#halfright").append('<img class="playerenemy" src="assets/images/quigon.jpeg">');
+        $("#halfright").css({ display: "block" });
+        player = true;
+        console.log(player);
+        if (player === true) {
+            console.log(player);
+            
+        }
+    }
+
+    function showPlayerDarth() {
+        $("#halfleft").append('<img class="playerselect" src="assets/images/darthmaul.jpeg">');
+        $("#halfleft").css({ display: "block" });
+        $("#halfright").append('<img class="playerenemy" src="assets/images/quigon.jpeg">');
+        $("#halfright").css({ display: "block" });
+        $("#halfright").append('<img class="playerenemy" src="assets/images/obiwan.jpg">');
+        $("#halfright").css({ display: "block" });
+        $("#halfright").append('<img class="playerenemy" src="assets/images/countdooku.jpg">');
+        $("#halfright").css({ display: "block" });
+        player = true;
+        console.log(player);
+        $("#arena").append('<img class="playerselect" src="assets/images/darthmaul.jpeg">');
+        if (player === true) {
+            console.log(player);
+          
+        }
+    }
--- assets/javascript/starwarsgame.js
+++ assets/javascript/starwarsgame.js
@@ -28,73 +28,9 @@ $(document).ready(function() {
     var charFour = new Character("DarthMaul", 150, 18, 30);
 
 
-    function fIntro() {
-        $("#theme")[0].volume = 0.5;
-        $("#theme")[0].play();
-    }
-
-    function fIntro2() {
-        $("#theme2")[0].play();
-    }
-
-    function showArena() {
-        $("#arena:hidden:first").fadeIn(2000);
-    }
-
-    function hoversound() {
-        $("#hoversound")[0].volume = 0.6;
-        $("#hoversound")[0].play();
-
-    }
-
-    function fDraw() {
-        $("#saberdraw")[0].play();
-    }
+    
 
 
-    function fBuzz() {
-        $("#saberbuzz")[0].play();
-    }
-
-    function sBuzz() {
-        $("#sabersbuzz")[0].play();
-    }
-
-    function fContact() {
-        $("#sabercontact")[0].play();
-    }
-
-    function fSheath() {
-        $("#sabersheath")[0].play();
-    }
-
-    function resetSound() {
-        $("#saberbuzz")[0].pause();
-        $("#sabersbuzz")[0].pause();
-        $("#saberbuzz")[0].currentTime = 0;
-        $("#sabersbuzz")[0].currentTime = 0;
-    }
-
-    function selectCharacter() {
-
-        function hoverEnlarge(idname) {
-
-            $(idname).on("mouseenter", function() {
-                $(idname).css({ border: "solid 5px yellow" });
-                hoversound();
-
-                $(idname).on("mouseleave", function() {
-                    $(idname).css({ border: "solid 1px white" });
-
-                });
-            });
-        }
-
-        hoverEnlarge(qui);
-        hoverEnlarge(darth);
-        hoverEnlarge(obi);
-        hoverEnlarge(count);
-    }
 
 
     // lightsaber sound effects
@@ -121,35 +57,7 @@ $(document).ready(function() {
         }
     };
 
-    // makes player move with arrow keys
-    $(document).keyup(function(e) {
-        switch (e.which) {
-            case 37:
-                $(".obiwan").animate({ left: "-=50px" }, "normal");
-        }
-    });
-    $(document).keyup(function(e) {
-        switch (e.which) {
-            case 38:
-                $(".obiwan").animate({ top: "-=50px" }, "normal");
-        }
-    });
-
-    $(document).keyup(function(e) {
-        switch (e.which) {
-            case 39:
-                $(".obiwan").animate({ left: "+=50px" }, "normal");
-        }
-    });
-
-    $(document).keyup(function(e) {
-        switch (e.which) {
-            case 40:
-                $(".obiwan").animate({ top: "+=50px" }, "normal");
-
-        }
-    });
-
+   
     // END OF FUNCTIONS 
 
 
@@ -166,100 +74,10 @@ $(document).ready(function() {
 
 
     console.log(player);
-    // selectCharacter();
+    selectCharacter();
 
 
-
-
-
-    // function darthEnemy(){
-
-    //     setInterval($(darth).remove();
-    // }
-
-
-
-
-
-
-
-
-
-
-    function showPlayerObi() {
-        $("#halfleft").append('<img class="playerselect" src="assets/images/obiwan.jpg">');
-        $("#halfleft").css({ display: "block" });
-        $("#halfright").append('<img class="playerenemy" src="assets/images/darthmaul.jpeg">');
-        $("#halfright").css({ display: "block" });
-        $("#halfright").append('<img class="playerenemy" src="assets/images/quigon.jpeg">');
-        $("#halfright").css({ display: "block" });
-        $("#halfright").append('<img class="playerenemy" src="assets/images/countdooku.jpg">');
-        $("#halfright").css({ display: "block" });
-        player = true;
-        console.log(player);
-        if (player === true) {
-            console.log(player);
-            console.log("asdfasfasdf");
-            setTimeout(showArena, 3000);
-        }
-    }
-
-
-    function showPlayerQui() {
-        $("#halfleft").append('<img class="playerselect" src="assets/images/quigon.jpeg">');
-        $("#halfleft").css({ display: "block" });
-        $("#halfright").append('<img class="playerenemy" src="assets/images/darthmaul.jpeg">');
-        $("#halfright").css({ display: "block" });
-        $("#halfright").append('<img class="playerenemy" src="assets/images/obiwan.jpg">');
-        $("#halfright").css({ display: "block" });
-        $("#halfright").append('<img class="playerenemy" src="assets/images/countdooku.jpg">');
-        $("#halfright").css({ display: "block" });
-        player = true;
-        console.log(player);
-        if (player === true) {
-            console.log(player);
-            console.log("asdfasfasdf");
-            setTimeout(showArena, 3000);
-        }
-    }
-
-    function showPlayerCount() {
-        $("#halfleft").append('<img class="playerselect" src="assets/images/countdooku.jpg">');
-        $("#halfleft").css({ display: "block" });
-        $("#halfright").append('<img class="playerenemy" src="assets/images/darthmaul.jpeg">');
-        $("#halfright").css({ display: "block" });
-        $("#halfright").append('<img class="playerenemy" src="assets/images/obiwan.jpg">');
-        $("#halfright").css({ display: "block" });
-        $("#halfright").append('<img class="playerenemy" src="assets/images/quigon.jpeg">');
-        $("#halfright").css({ display: "block" });
-        player = true;
-        console.log(player);
-        if (player === true) {
-            console.log(player);
-            console.log("asdfasfasdf");
-            setTimeout(showArena, 3000);
-        }
-    }
-
-    function showPlayerDarth() {
-        $("#halfleft").append('<img class="playerselect" src="assets/images/darthmaul.jpeg">');
-        $("#halfleft").css({ display: "block" });
-        $("#halfright").append('<img class="playerenemy" src="assets/images/quigon.jpeg">');
-        $("#halfright").css({ display: "block" });
-        $("#halfright").append('<img class="playerenemy" src="assets/images/obiwan.jpg">');
-        $("#halfright").css({ display: "block" });
-        $("#halfright").append('<img class="playerenemy" src="assets/images/countdooku.jpg">');
-        $("#halfright").css({ display: "block" });
-        player = true;
-        console.log(player);
-        $("#arena").append('<img class="playerselect" src="assets/images/darthmaul.jpeg">');
-        if (player === true) {
-            console.log(player);
-            console.log("asdfasfasdf");
-            setTimeout(showArena, 3000);
-        }
-    }
-
+    
 
 
 
@@ -279,7 +97,7 @@ $(document).ready(function() {
         $(count).fadeOut(1000);
         $(obi).fadeOut(1000);
         setTimeout(showPlayerQui, 1000);
-        
+
     });
 
     $(count).on("click", function() {
@@ -301,10 +119,9 @@ $(document).ready(function() {
     });
     console.log(player);
 
-if( player===true)
-{
-    console.log("yeah its true!!!")
-}
+    if (player === true) {
+        console.log("yeah its true!!!")
+    }
 
 
 });
