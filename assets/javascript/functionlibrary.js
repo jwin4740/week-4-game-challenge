36ac8799395f59ad6375ec4e6659d152e06a94b4
James <jwin4740@gmail.com>
2016-12-26 13:21:42 -0800

    door

--- assets/javascript/functionlibrary.js
+++ assets/javascript/functionlibrary.js
@@ -1,224 +1,119 @@
-
-
-
 function fIntro() {
-        $("#theme")[0].volume = 0.5;
-        $("#theme")[0].play();
+    $("#theme")[0].volume = 0.5;
+    $("#theme")[0].play();
+}
+
+function fIntro2() {
+    $("#theme2")[0].play();
+}
+
+function showArena() {
+    $("#arena:hidden:first").fadeIn(2000);
+}
+
+function hoversound() {
+    $("#hoversound")[0].volume = 0.6;
+    $("#hoversound")[0].play();
+}
+
+function fDraw() {
+    $("#saberdraw")[0].play();
+}
+
+function fBuzz() {
+    $("#saberbuzz")[0].play();
+}
+
+function sBuzz() {
+    $("#sabersbuzz")[0].play();
+}
+
+function fContact() {
+    $("#sabercontact")[0].play();
+}
+
+function fSheath() {
+    $("#sabersheath")[0].play();
+}
+
+function resetSound() {
+    $("#saberbuzz")[0].pause();
+    $("#sabersbuzz")[0].pause();
+    $("#saberbuzz")[0].currentTime = 0;
+    $("#sabersbuzz")[0].currentTime = 0;
+}
+
+
+// makes player move with arrow keys
+$(document).keyup(function(e) {
+    switch (e.which) {
+        case 37:
+            $(".obiwan").animate({ left: "-=50px" }, "normal");
     }
-
-    function fIntro2() {
-        $("#theme2")[0].play();
+});
+$(document).keyup(function(e) {
+    switch (e.which) {
+        case 38:
+            $(".obiwan").animate({ top: "-=50px" }, "normal");
     }
+});
 
-    function showArena() {
-        $("#arena:hidden:first").fadeIn(2000);
+$(document).keyup(function(e) {
+    switch (e.which) {
+        case 39:
+            $(".obiwan").animate({ left: "+=50px" }, "normal");
     }
+});
 
-    function hoversound() {
-        $("#hoversound")[0].volume = 0.6;
-        $("#hoversound")[0].play();
-
-    }
-
-    function fDraw() {
-        $("#saberdraw")[0].play();
-    }
-
-
-    function fBuzz() {
-        $("#saberbuzz")[0].play();
-    }
-
-    function sBuzz() {
-        $("#sabersbuzz")[0].play();
-    }
-
-    function fContact() {
-        $("#sabercontact")[0].play();
-    }
-
-    function fSheath() {
-        $("#sabersheath")[0].play();
-    }
-
-    function resetSound() {
-        $("#saberbuzz")[0].pause();
-        $("#sabersbuzz")[0].pause();
-        $("#saberbuzz")[0].currentTime = 0;
-        $("#sabersbuzz")[0].currentTime = 0;
-    }
-
-        function selectCharacter() {
-
-        function hoverEnlarge(idname) {
-
-            $(idname).on("mouseenter", function() {
-                $(idname).addClass("hover");
-                hoversound();
-
-                $(idname).on("mouseleave", function() {
-                    $(idname).removeClass("hover");
-
-                });
-            });
-        }
-
-        hoverEnlarge("#playerone");
-        hoverEnlarge("#playertwo");
-        hoverEnlarge("#playerthree");
-        hoverEnlarge("#playerfour");
-    }
-
-     // makes player move with arrow keys
-    $(document).keyup(function(e) {
-        switch (e.which) {
-            case 37:
-                $(".obiwan").animate({ left: "-=50px" }, "normal");
-        }
-    });
-    $(document).keyup(function(e) {
-        switch (e.which) {
-            case 38:
-                $(".obiwan").animate({ top: "-=50px" }, "normal");
-        }
-    });
-
-    $(document).keyup(function(e) {
-        switch (e.which) {
-            case 39:
-                $(".obiwan").animate({ left: "+=50px" }, "normal");
-        }
-    });
-
-    $(document).keyup(function(e) {
-        switch (e.which) {
-            case 40:
-                $(".obiwan").animate({ top: "+=50px" }, "normal");
-
-        }
-    });
-
-
-function showPlayerObi() {
-        $("#halfleft").append('<img class="playerselect" src="assets/images/obiwan.jpg">');
-        $("#halfleft").css({ display: "block" });
-        $("#halfright").append('<img class="playerenemy" src="assets/images/darthmaul.jpeg">');
-        $("#halfright").css({ display: "block" });
-        $("#halfright").append('<img class="playerenemy" src="assets/images/quigon.jpeg">');
-        $("#halfright").css({ display: "block" });
-        $("#halfright").append('<img class="playerenemy" src="assets/images/countdooku.jpg">');
-        $("#halfright").css({ display: "block" });
-        $("#playerbox").empty();
-        player = true;
-        console.log(player);
-        if (player === true) {
-            console.log(player);
-            
-        }
-    }
-
-
-    function showPlayerQui() {
-        $("#halfleft").append('<img class="playerselect" src="assets/images/quigon.jpeg">');
-        $("#halfleft").css({ display: "block" });
-        $("#halfright").append('<img class="playerenemy" src="assets/images/darthmaul.jpeg">');
-        $("#halfright").css({ display: "block" });
-        $("#halfright").append('<img class="playerenemy" src="assets/images/obiwan.jpg">');
-        $("#halfright").css({ display: "block" });
-        $("#halfright").append('<img class="playerenemy" src="assets/images/countdooku.jpg">');
-        $("#halfright").css({ display: "block" });
-        player = true;
-        console.log(player);
-        if (player === true) {
-            console.log(player);
-           
-        }
-    }
-
-    function showPlayerCount() {
-        $("#halfleft").append('<img class="playerselect" src="assets/images/countdooku.jpg">');
-        $("#halfleft").css({ display: "block" });
-        $("#halfright").append('<img class="playerenemy" src="assets/images/darthmaul.jpeg">');
-        $("#halfright").css({ display: "block" });
-        $("#halfright").append('<img class="playerenemy" src="assets/images/obiwan.jpg">');
-        $("#halfright").css({ display: "block" });
-        $("#halfright").append('<img class="playerenemy" src="assets/images/quigon.jpeg">');
-        $("#halfright").css({ display: "block" });
-        player = true;
-        console.log(player);
-        if (player === true) {
-            console.log(player);
-            
-        }
-    }
-
-    function showPlayerDarth() {
-        $("#halfleft").append('<img class="playerselect" src="assets/images/darthmaul.jpeg">');
-        $("#halfleft").css({ display: "block" });
-        $("#halfright").append('<img class="playerenemy" src="assets/images/quigon.jpeg">');
-        $("#halfright").css({ display: "block" });
-        $("#halfright").append('<img class="playerenemy" src="assets/images/obiwan.jpg">');
-        $("#halfright").css({ display: "block" });
-        $("#halfright").append('<img class="playerenemy" src="assets/images/countdooku.jpg">');
-        $("#halfright").css({ display: "block" });
-        player = true;
-        console.log(player);
-        $("#arena").append('<img class="playerselect" src="assets/images/darthmaul.jpeg">');
-        if (player === true) {
-            console.log(player);
-          
-        }
-    }
-
-
- document.onkeyup = function(event) {
-        var userKey = event.key;
-        if (userKey === "d") {
-            fDraw();
-            setTimeout(fBuzz, 1000);
-            setTimeout(sBuzz, 2000);
-        }
-
-        if (userKey === "s") {
-            fContact();
-        }
-
-
-        if (userKey === "w") {
-            fSheath();
-        }
-
-        if (userKey === "w") {
-            setTimeout(fSheath, 500);
-            resetSound();
-        }
-    };
-
-    function attackaction (event){
-       
-                currentdefender.hP = currentdefender.hP - attacker.aP;
-                attacker.aP = attacker.aP + 15;
-                attacker.hP = attacker.hP - currentdefender.cAP;
-                $('.' + attacker.class).html(attacker.hP + " hP");
-                $('.' + currentdefender.class).html(currentdefender.hP + " hP");
-                console.log("attacker health is now : " + attacker.hP);
-                console.log("currentdefender health is now: " + currentdefender.hP);
-                $("#gamewindow").html("You attacked " + currentdefender.name + "for " + attacker.aP + " damage!!" + "<br>");
-                $("#gamewindow").append(currentdefender.name + "attacked you for " + currentdefender.cAP + " damage!!" + "<br>");
-                $("#gamewindow").append("Your hP is now: " + attacker.hP + "<br>");
-                $("#gamewindow").append(currentdefender.name + "'s hP is now: " + currentdefender.hP);
-
-                if (currentdefender.hP <= 0) {
-                    $("#defenderbox").append("You have defeated your opponent!!! Choose your next opponent");
-                    checkpoint3 = false;
-                    checkpoint2 = false;
-
-                }
-                if (attacker.hP <= 0) {
-                    $("#defenderbox").append("You have been defeated!!!");
-                    checkpoint3 = false;
-                    checkpoint2 = false;
-                }
-            
+$(document).keyup(function(e) {
+    switch (e.which) {
+        case 40:
+            $(".obiwan").animate({ top: "+=50px" }, "normal");
 
     }
+});
+
+document.onkeyup = function(event) {
+    var userKey = event.key;
+    if (userKey === "d") {
+        fDraw();
+        setTimeout(fBuzz, 1000);
+        setTimeout(sBuzz, 2000);
+    }
+    if (userKey === "s") {
+        fContact();
+    }
+    if (userKey === "w") {
+        fSheath();
+    }
+    if (userKey === "w") {
+        setTimeout(fSheath, 500);
+        resetSound();
+    }
+};
+
+function attackaction(event) {
+    currentdefender.hP = currentdefender.hP - attacker.aP;
+    attacker.aP = attacker.aP + 15;
+    attacker.hP = attacker.hP - currentdefender.cAP;
+    $('.' + attacker.class).html(attacker.hP + " hP");
+    $('.' + currentdefender.class).html(currentdefender.hP + " hP");
+    console.log("attacker health is now : " + attacker.hP);
+    console.log("currentdefender health is now: " + currentdefender.hP);
+    $("#gamewindow").html("You attacked " + currentdefender.name + "for " + attacker.aP + " damage!!" + "<br>");
+    $("#gamewindow").append(currentdefender.name + "attacked you for " + currentdefender.cAP + " damage!!" + "<br>");
+    $("#gamewindow").append("Your hP is now: " + attacker.hP + "<br>");
+    $("#gamewindow").append(currentdefender.name + "'s hP is now: " + currentdefender.hP);
+
+    if (currentdefender.hP <= 0) {
+        $("#defenderbox").append("You have defeated your opponent!!! Choose your next opponent");
+        checkpoint3 = false;
+        checkpoint2 = false;
+
+    }
+    if (attacker.hP <= 0) {
+        $("#defenderbox").append("You have been defeated!!!");
+        checkpoint3 = false;
+        checkpoint2 = false;
+    }
+}
--- assets/javascript/starwars.js
+++ assets/javascript/starwars.js
@@ -1,6 +1,4 @@
 //global variables
-
-
 var cArray = [];
 var attacker;
 var currentdefender;
@@ -9,18 +7,11 @@ var checkpoint1 = false;
 var checkpoint2 = false;
 var checkpoint3 = false;
 var tempval;
-
-
+var clicksoundinitial = true;
+var clicksounddefender = true;
 
 
 $(document).ready(function() {
-
-    function fIntro() {
-        $("#theme")[0].volume = 0.5;
-        $("#theme")[0].play();
-    }
-    // DEFINING FUNCTIONS AND OBJECTS go to line xx to see how game starts
-
 
     // prototype object
     function Character(name, hP, aP, cAP, id, num, imagesource, hpclass) {
@@ -28,11 +19,11 @@ $(document).ready(function() {
         this.hP = hP;
         this.aP = aP;
         this.cAP = cAP;
-        this.ele = '<div ' + id + 'class="player" data-value="' + num + '"><h4>' + this.name + '</h4><img class="pimage" src="assets/images/' + imagesource + '<h4 class=' + hpclass + '>' + this.hP + '  hP</div>';
+        this.ele = '<div ' + id + 'class="player hovers" data-value="' + num + '"><h4>' + this.name + '</h4><img class="pimage" src="assets/images/' + imagesource + '<h4 class=' + hpclass + '>' + this.hP + '  hP</div>';
         this.class = hpclass;
     };
 
-
+    // make Character objects for each player
     var charOne = new Character("ObiwanKenobi", 175, 12, 15, 'id="obi" ', 1, 'obiwan.jpg">', 'obiclass');
     var charTwo = new Character("QuigonGinn", 200, 14, 20, 'id="qui" ', 2, 'quigon.jpeg">', 'quiclass');
     var charThree = new Character("CountDooku", 250, 16, 25, 'id="count" ', 3, 'countdooku.jpg">', 'countclass');
@@ -50,19 +41,40 @@ $(document).ready(function() {
 
     // user chooses their player which appends to the attacker box, while the remaining players get appended
     // to the defenderondeck box
+
+    // fIntro();
+
     function setup() {
         $("#choosebox").append(charOne.ele).append(charTwo.ele).append(charThree.ele).append(charFour.ele);
     }
-
-
     setup();
-    fIntro();
+    $('.player').on("mouseover", function() {
+        if ($(this).hasClass("attacker"))
+        {
+            return;
+        }
 
+
+        if ($(this).hasClass("currentdefender"))
+        {
+            return;
+        }
+
+        if ($(this).hasClass("defender") && clicksoundinitial)
+        {
+            hoversound();
+        }
+
+        if(clicksoundinitial)
+        {
+            hoversound();
+        }
+
+    });
     $('.player').on("click", function() {
         if (!checkpoint1) {
             $(this).siblings().addClass("defender").appendTo("#defenderondeckbox");
-            $(this).addClass("attacker").appendTo("#attackerbox");
-
+            $(this).addClass("attacker").removeClass("hovers").appendTo("#attackerbox");
             tempval = $(this).attr("data-value");
             attacker = cArray[tempval - 1];
             console.log(attacker);
@@ -73,16 +85,18 @@ $(document).ready(function() {
 
     $("#defenderondeckbox").on("click", ".player.defender", function() {
         if (!checkpoint2) {
+            
             $("#defenderbox").html(this);
+            $(this).removeClass("hovers").addClass("currentdefender");
             tempval = $(this).attr("data-value");
             currentdefender = cArray[tempval - 1];
             currentdefender.hP = cArray[tempval - 1].hP;
             console.log(currentdefender.hP);
-
             console.log(currentdefender);
             checkpoint2 = true;
             $("#defenderbox").append();
             checkpoint3 = true;
+            
         }
     });
 
